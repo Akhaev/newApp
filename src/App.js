744c31c7ef0078ca9577b6ba698a4b323b317e21
Akhaev <albertakhaev@gmail.com>
2024-05-19 14:12:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const array = [22, 0, -45, 1001, 8, -6, -0.1, 101, 2.5, -55555];
+const positive = array.filter(item => {
+    if (item > 0) {
+        return item
+    }
+})
+
+console.log(positive);
+
+
+const messages = [
+    {message: 'hello', error: true},  
+    {message: 'javascript', error: false},  
+    {message: 'expovisiov', error: true},  
+    {message: 'react', error: true},  
+    {message: 'angular', error: false}, 
+    {message: 'es6', error: false}, 
+]
+
+console.log(messages.filter(item => {
+    if (item.error == false) {
+        return item
+    }
+})); 
+
+
+const words= ['экспо', 'js', 'react', 'css', 'angular', 'vue', 'html']
+console.log(words.map(item => {
+    if (item.length < 5) {
+        return item = '*'
+    }else{
+        return item
+    }
+})); 
+
+
+let sendMessage = (message) => {
+    /*
+    * тут какой-то большой код для отправки сообщения
+    */
 }
 
-export default App;
+let half = (number) => {
+    return number / 2;
+}
+
+let showConsole = () => {
+    console.log('Экспо');
+}
+
+let concatWords = (first, second) => {
+    return first + second
+}
+
+function justText() {
+    'expo'
+} 
+
+function logging(text) {
+    console.log(text)
+} 
+
+function add(x,y) {
+    const z = 3;
+  
+    return z * x * y;
+}
+
+function onlyPositive(number) {
+    if(number < 0) {
+      return false;
+    }
+  
+    return true;
+}
+
+function App() {
+}
+
+export default App
